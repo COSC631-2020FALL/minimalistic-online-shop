a52a3b3c5dcfe0e04d91ad2f46cbcd51dfefdfc3
greg <goyafuso.voided@gmail.com>
2020-10-24 16:14:21 +0000

    user table, nullable addresses

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address_1');
-            $table->string('address_2');
+            $table->string('address_1')->nullable();
+            $table->string('address_2')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
