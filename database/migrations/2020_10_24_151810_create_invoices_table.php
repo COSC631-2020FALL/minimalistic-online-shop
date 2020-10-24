1adc09253e8ef12d3d40f3db7d0bf695b3c9e3fa
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-10-24 11:23:42 -0400

    invoice migration created

---
+++ database/migrations/2020_10_24_151810_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('buyer_id'); // user_id
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
