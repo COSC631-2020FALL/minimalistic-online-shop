2ea80237fd14d700149deea6571c24751e5efc70
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-10-25 22:22:41 -0400

    bugs fixed

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,11 +8,11 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     $owner_id = App\User::all()->random()->id;
 	$tag_id = App\Tag::all()->random()->id;
-	
+
     return [
         'name' => $faker->name,
         'description' => $faker->realText(255),
-        'img_url' => $faker->img_url,
+        'img_url' => $faker->imageUrl(),
         'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 20),
         'owner_id' => $owner_id,
         'tag_id' => $tag_id,
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -6,15 +6,14 @@ use App\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
-	
+
     $review_id = App\User::all()->random()->id;
     $product_id =  App\Product::all()->random()->id;
 
     return [
-        //
-	'reviewer_id' => $review_id;   
-	'product_id'  => $product_id;
-	'review'      => $faker-> realText($maxNbChars = 200, $indexSize = 2);
-        'rating'      => $faker->random_int(0,5);
+        'reviewer_id' => $review_id,
+        'product_id'  => $product_id,
+        'review'      => $faker-> realText($maxNbChars = 200, $indexSize = 2),
+        'rating'      => $faker->random_int(0,5),
     ];
 });
