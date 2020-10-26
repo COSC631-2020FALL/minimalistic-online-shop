626b6467078371fbc293f0cef3cd753e9f81d240
eriksali <43148041+eriksali@users.noreply.github.com>
2020-10-25 20:56:03 -0400

    Update ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,16 @@ use App\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
+    $owner_id = App\User::all()->random()->id;
+	$tag_id = App\Tag::all()->random()->id;
+	
     return [
-        //
+        'name' => $faker->name,
+        'description' => $faker->realText(255),
+        'img_url' => $faker->img_url,
+        'price' => $faker->price,
+        'owner_id' => $owner_id,
+        'tag_id' => $tag_id,
     ];
 });
+
