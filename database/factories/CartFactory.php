317ef8ffe229799f04a6d28cd88e06529018e743
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-10-25 20:06:11 -0400

    user and cart factory created

--- database/factories/CartFactory.php
+++ database/factories/CartFactory.php
@@ -6,7 +6,12 @@ use App\Cart;
 use Faker\Generator as Faker;
 
 $factory->define(Cart::class, function (Faker $faker) {
+    $user_id = App\User::all()->random()->id;
+    $order_id = App\Order::all()->random()->id;
+
     return [
-        //
+        'user_id' => $user_id,
+        'orders_id' => $order_id,
+        'quantity' => $faker->rand()
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,8 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'address_1' => $faker->address,
+        'address_2' => $faker->streetAddress,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
