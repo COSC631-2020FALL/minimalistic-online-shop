257ae561c6e5776f070c9cb88f6859f512fad639
greg <goyafuso.voided@gmail.com>
2020-10-27 23:28:59 +0000

    InvoiceFactory

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -6,7 +6,11 @@ use App\Invoice;
 use Faker\Generator as Faker;
 
 $factory->define(Invoice::class, function (Faker $faker) {
+    $buyer_id = App\User::all()->random()->id;
+
+	
     return [
-        //
+        'buyer_id' => $buyer_id,
+        'total'    => $faker->random()
     ];
 });
