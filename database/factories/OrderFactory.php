0d156101bfcdb390e5761ee5223b9652401d3a82
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-03 20:17:53 -0800

    testing bug fixed

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -7,13 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Order::class, function (Faker $faker) {
 
-    $product_id = App\Product::all()->random()->id;
-    $order_id   = App\Invoice::all()->random()->id;
     $user_id    = App\User::all()->random()->id;
-    
+    $product_id = App\Product::all()->random()->id;
+    $invoice_id   = App\Invoice::all()->random()->id;
+
     return [
         'product_id' => $product_id,
-	    'invoice_id' => $order_id,
+	    'invoice_id' => $invoice_id,
 	    'user_id'    => $user_id
     ];
 });
---
+++ tests/Unit/RelationshipTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Order;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+
+class RelationshipTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_user_has_many_orders(){
+
+        // generate data (user orders)
+        $user = factory(User::class, 3)->create()->random();
+        factory(Order::class, 3)->create(['user_id' => $user->id]);
+
+        // run the function you wanna test
+        $orders = $user->orders;
+
+        // test/assert the if the outcome is as expected
+        $this->assertGreaterThanOrEqual(3, $orders->count());
+    }
+}
