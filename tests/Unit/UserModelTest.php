242eac3b0f3e99de4c8f45fd985b62127c5de435
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-07 18:11:14 -0800

    random data removed

--- tests/Unit/UserModelTest.php
+++ tests/Unit/UserModelTest.php
@@ -3,37 +3,40 @@
 namespace Tests\Unit;
 
 use App\Order;
+use App\Product;
+use App\Review;
 use App\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-
 
 class UserModelTest extends TestCase
 {
 
-    public function test_user_has_many_orders() {
+    public function test_a_user_has_many_orders() {
         //grab rand user
-        $user = User::all()->random();
+        $user = factory(User::class)->create();
+        factory(Order::class, 3)->create(['user_id' => $user->id]);
         $orders = $user->orders;
         // test/assert the if the outcome is as expected
-        $this->assertGreaterThanOrEqual(0, $orders->count());
+        $this->assertEquals(3, $orders->count());
     }
 
     public function test_user_has_many_products() {
         //grab rand user
-        $user = User::all()->random();
+        $user = factory(User::class)->create();
+        factory(Product::class, 3)->create(['owner_id' => $user->id]);
         $products = $user->products;
-        // test/assert the if the outcome is as expected
-        $this->assertGreaterThanOrEqual(0, $products->count());
+        // test/assert if the outcome is as expected
+        $this->assertEquals(3, $products->count());
     }
 
     public function test_user_has_many_reviews() {
-        //grab rand user
-        $user = User::all()->random();
+        //create test data
+        $user = factory(User::class)->create();
+        factory(Review::class, 3)->create(['reviewer_id' => $user->id]);
         $reviews = $user->reviews;
+
         // test/assert the if the outcome is as expected
-        $this->assertGreaterThanOrEqual(0, $reviews->count());
+        $this->assertEquals(3, $reviews->count());
     }
     /*
     public function test_user_has_one_cart() {
