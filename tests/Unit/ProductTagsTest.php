f2e5cb4defd83caf7e0cd1a7fef4699f15548b1b
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-10 07:07:14 -0800

    updated

--- tests/Unit/ProductTagsTest.php
+++ tests/Unit/ProductTagsTest.php
@@ -10,29 +10,5 @@ use Tests\TestCase;
 
 class ProductTagsTest extends TestCase
 {
-    function test_tag_has_many_products(){
-        // generate data
-        $tag = factory(Tag::class)->create();
-
-        $product1 = factory(Product::class)->create();
-        $product2 = factory(Product::class)->create();
-        $product3 = factory(Product::class)->create();
-
-        factory(ProductTags::class)->create([
-            'product_id' => $product1->id,
-            'tag_id' => $tag->id
-        ]);
-
-        factory(ProductTags::class)->create([
-            'product_id' => $product2->id,
-            'tag_id' => $tag->id
-        ]);
-
-        factory(ProductTags::class)->create([
-            'product_id' => $product3->id,
-            'tag_id' => $tag->id
-        ]);
-        // test
-        $this->assertGreaterThanOrEqual(3, $tag->products->count());
-    }
+    
 }
--- tests/Unit/TagModelTest.php
+++ tests/Unit/TagModelTest.php
@@ -2,17 +2,37 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Product;
+use App\ProductTags;
+use App\Tag;
+use Tests\TestCase;
 
 class TagModelTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    function test_tag_has_many_products()
     {
-        $this->assertTrue(true);
+        // generate data
+        $tag = factory(Tag::class)->create();
+
+        $product1 = factory(Product::class)->create();
+        $product2 = factory(Product::class)->create();
+        $product3 = factory(Product::class)->create();
+
+        factory(ProductTags::class)->create([
+            'product_id' => $product1->id,
+            'tag_id' => $tag->id
+        ]);
+
+        factory(ProductTags::class)->create([
+            'product_id' => $product2->id,
+            'tag_id' => $tag->id
+        ]);
+
+        factory(ProductTags::class)->create([
+            'product_id' => $product3->id,
+            'tag_id' => $tag->id
+        ]);
+        // test
+        $this->assertGreaterThanOrEqual(3, $tag->products->count());
     }
 }
