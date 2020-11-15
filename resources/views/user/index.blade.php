adfa1a65e188d89a3b4dc5ce03facf978697a7dc
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-15 13:40:46 -0800

    index fixed

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,10 +12,31 @@
                     <div class="card-body">
                        <table>
 
-                            @foreach ($users as $user)
-                                <p>This is user {{ $user->name }}</p>
-                            @endforeach
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
 
+                            @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->name}}</td>
+                                <td>{{ $user->email}}</td>
+                                <td>{{ $user->address_1}}</td>
+                                <td>
+
+                                    <form action="{{ route('users.destroy', $user->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-primary">DELETE</button>
+                                    </form>
+
+                                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary pull-right">EDIT</a>
+
+                                </td>
+                            </tr>
+                            @endforeach
                        </table>
                     </div>
                 </div>
