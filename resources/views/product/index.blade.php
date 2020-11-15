a828ada3fe1c55ab5a8b156d0079f85a022cf9a5
greg <goyafuso.voided@gmail.com>
2020-11-15 20:55:52 +0000

    table for products all  view

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,9 +2,37 @@
 
 @section('content')
 
-    @foreach ($products as $product)
-        <p>This is a product {{ $product->name }}</p>
-    @endforeach
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Products</div>
 
+                    <div class="card-body">
+                       <table>
+                        <tr>
+                            <th>ProductName</th>
+                            <th>Description</th>
+                            <th>Picture</th>
+                            <th>Price</th>
+                            <th>Seller</th>
+                            <th>Tags</th>
+                        </tr>
+                        @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->description }}</td>
+                            <td><img style="width:8em;height:8em;" src='{{ $product->img_url }}' /></td>
+                            <td>{{ $product->price }}</td>
+                            <td>{{ $product->owner_id }}</td>
+                            <td>{{ $product->tag_id }}</td>
+                        </tr>
+                        @endforeach
+                       </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
