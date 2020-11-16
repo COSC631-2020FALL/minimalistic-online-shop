e5a5a17ee03e602d01b549f8d1a169fac171f4a1
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-16 15:17:09 -0800

    gitignore and user controller updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,6 +96,6 @@ class UserController extends Controller
     {
         $user->delete();
 
-        return redirect(route('users.index'));
+        return redirect()->route('users.index');
     }
 }
