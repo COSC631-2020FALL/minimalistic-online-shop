dc612915f7821197da9afdc15beb4e80e23d08f7
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-22 15:16:07 -0800

    order migration updated

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -8,11 +8,9 @@ use Faker\Generator as Faker;
 $factory->define(Order::class, function (Faker $faker) {
 
     $user_id    = App\User::all()->random()->id;
-    $product_id = App\Product::all()->random()->id;
 
 
     return [
-        'product_id' => $product_id,
 	    'user_id'    => $user_id
     ];
 });
