4e9701b4b8bc5f6faa4c66157c4a1e03119e5d3b
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-23 19:55:52 -0800

    product show updated

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,39 +1,53 @@
 <div class="card">
-            <div class="card-header"> <a href="{{ route('products.show', $product->id)}}"> Product | {{$product->name}} </a> </div>
-               <div class="card-body">
+    <div class="card-header"> <a href="{{ route('products.show', $product->id)}}"> Product | {{$product->name}} </a> </div>
+        <div class="card-body">
 
-                  <div class="card-body">
+            <div class="card-body">
 
-                    <div class="col-md-6 float-left">
-                        <a href="{{ route('products.show', $product->id)}}">
-                            <img class="rounded float-left" style="position: relative; width:100%;height:100%;" src='{{ $product->img_url }}' />
-                        </a>
-                    </div>
+            <div class="col-md-6 float-left">
+                <a href="{{ route('products.show', $product->id)}}">
+                    <img class="rounded float-left" style="position: relative; width:100%;height:100%;" src='{{ $product->img_url }}' />
+                </a>
+            </div>
 
 
-                    <div class="col-md-6 float-right">
+            <div class="col-md-6 float-right">
 
-                        <form action="{{route('carts.store', $product->id)}}" method="post">
-                            @csrf
-                            <span class="row">{{ $product->name }}</span>
-                            <br>
-                            <span class="row">{{ $product->description }}</span>
-                            <br>
-                            <span class="row">$ {{ $product->price }}</span>
-                            <br>
-                            <span class="row">
-                                <input type="range" name="quantity" value="{{$product->quantity}}">
-                            </span>
-                            <br>
-                            <span class="row">
+                <form action="{{route('carts.store', $product->id)}}" method="post">
+                    @csrf
+                    <span class="row">{{ $product->name }}</span>
+                    <br>
+                    <span class="row">{{ $product->description }}</span>
+                    <br>
+                    <span class="row">$ {{ $product->price }}</span>
+                    <br>
+                    <span class="row">
+                        <input type="range" name="quantity" value="{{$product->quantity}}">
+                    </span>
+                    <br>
+                    <span class="row">
 
+                        <div class="btn-toolbar justify-content-between">
+
+                            <div class="btn-group mr-2">
                                 <button type="submit" class="btn btn-outline-primary">ADD TO CART</button>
+                            </div>
 
-                            </span>
-                        </form>
-                    </div>
+                            @auth
+                                @isowner($product)
+                                    <div class="btn-group">
+                                        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-outline-primary">EDIT</a>
+                                    </div>
+                                @endisowner
+                            @endauth
 
-                  </div>
-               </div>
-         </div>
-         <hr>
+
+                        </div>
+
+                    </span>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
