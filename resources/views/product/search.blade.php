ca39044e7a00c3112def3c54c3abf1dc80c1dab0
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-27 16:16:46 -0800

    css update

--- resources/views/product/search.blade.php
+++ resources/views/product/search.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 @if(count($products) == 0)
-                No search results found
+                <div class="text-center">No search results match your query</div>
                 @else
                     @each('components.product', $products, 'product')
                 @endif
