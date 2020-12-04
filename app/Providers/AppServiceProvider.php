7fc79f1de1263b752bd3e14e3c7acf1e49618e88
eriksali <43148041+eriksali@users.noreply.github.com>
2020-12-04 17:13:30 -0500

    update ProductFactory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
         Blade::if('isadmin', function () {
             return Auth::user()->is_admin === true;
         });
+		Schema::defaultStringLength(191);
 
     }
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -15,6 +15,7 @@ $factory->define(Product::class, function (Faker $faker) {
         'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 20),
         'owner_id' => $owner_id,
         'quantity' => rand($min = 1, $max = 50),
+		'cat_id' => rand($min = 1, $max = 4),
     ];
 });
 
