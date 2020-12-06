eb09ab422952dcbf846a4fc7577c7fbb6df2e13c
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-06 12:25:22 -0800

    admin link added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,6 +82,9 @@
 
                                     <a class="dropdown-item" href="{{ route('products.index').'?inventory=true' }}">Inventory</a>
 
+                                    @isadmin
+                                        <a class="dropdown-item" href="{{ '#' }}">Admin</a>
+                                    @endisadmin
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
