946a22ad22eac31ddf3a69b744a5a76357a64111
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-07 12:28:55 -0800

    partial file created

---
+++ resources/views/components/categories.blade.php
@@ -0,0 +1,16 @@
+<nav >
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Shoes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Shirts</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Books</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Electronics</a>
+        </li>
+    </ul>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,22 +93,7 @@
         </nav>
 
 
-		<nav >
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Shoes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Shirts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Books</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Electronics</a>
-                </li>
-            </ul>
-        </nav>
+		@include('components.categories')
 
 
         <main class="py-4">
