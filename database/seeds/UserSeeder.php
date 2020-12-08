df2e9b453638d647488a79056b84ac91863a230b
greg <goyafuso.voided@gmail.com>
2020-12-08 22:59:27 +0000

    non admin user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,5 +15,8 @@ class UserSeeder extends Seeder
         factory(App\User::class)->create(['is_admin' => true, 'name' => 'Tangeni', 'email' => 'tangeni@gmail.com', 'password' => bcrypt('secret')]);
         factory(App\User::class)->create(['is_admin' => true, 'name' => 'Greg', 'email' => 'greg@gmail.com', 'password' => bcrypt('secret')]);
         factory(App\User::class)->create(['is_admin' => true, 'name' => 'Li', 'email' => 'li@gmail.com', 'password' => bcrypt('secret')]);
+        factory(App\User::class)->create(['is_admin' => false, 'name' => 'Tangeni', 'email' => 'tangeni2@gmail.com', 'password' => bcrypt('secret')]);
+        factory(App\User::class)->create(['is_admin' => false, 'name' => 'Greg', 'email' => 'greg2@gmail.com', 'password' => bcrypt('secret')]);
+        factory(App\User::class)->create(['is_admin' => false, 'name' => 'Li', 'email' => 'li2@gmail.com', 'password' => bcrypt('secret')]);
     }
 }
