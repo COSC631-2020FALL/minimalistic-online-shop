af48fc3ed445b5ce4501710d7c8738bb76f8a69d
eriksali <43148041+eriksali@users.noreply.github.com>
2020-12-08 18:53:28 -0500

    Add files via upload

---
+++ app.blade(1).php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Shop') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+	<style>
+		ul#menu li {
+		  display:inline;
+		  padding: 10px;
+		}
+
+	</style>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Shoppers') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+
+                    <form action="{{ route('search') }}" method="GET" class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">@Include('icons.search')</span>
+                        </div>
+                        <input name="search" type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
+                    </form>
+
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('carts.index') }}">@include('icons.cart')</a>
+                        </li>
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <a class="dropdown-item" href="{{ route('users.show', Auth::user()->id )}}">
+                                        Profile
+                                    </a>
+
+
+                                    <a class="dropdown-item" href="{{ route('products.index').'?inventory=true' }}">Inventory</a>
+
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>		
+		</nav>	
+        <nav>
+            <div class="container">
+				<div class="text-center">
+				</br>
+					<ul id="menu">
+						<li><a href="{{route('search')}}">Home</a></li>
+                    	@foreach($categories as $category)
+							<li ><a href="{{route('search', ['search'=>$category->id])}}">{{$category->name}}</a></li>
+						@endforeach	
+                        <li><a href="{{route('products.index')}}">Browse All</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>				
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        <footer>
+            @yield('footer')
+        </footer>
+    </div>
+</body>
+</html>
