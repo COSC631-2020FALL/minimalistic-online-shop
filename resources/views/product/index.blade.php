8701c4fef0af19c4077a133dbbd5b1e0361507d4
greg <goyafuso.voided@gmail.com>
2020-12-10 05:42:03 +0000

    auth added to product.index

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -8,19 +8,23 @@
             <div class="col-md-8">
 
                 @include('components.alert')
-
-                @if ($inventory == true)
+                
+                @if($inventory == true)
+                @auth
                     <div>
                         <a href="{{ route("products.create") }}">
                             <button class="btn btn-primary" type="submit">ADD PRODUCTS</button>
                         </a>
                     </div>
                     <br>
+                @endauth
                 @endif
                 @if($inventory == true)
+                    @auth
                     @foreach(Auth::user()->products()->with('category')->has('category')->get() as $product)
                         @include('components.product',['product'=>$product,'inventory'=>$inventory])
                     @endforeach
+                    @endauth
                 @else       
                     @foreach(\App\Product::with('category')->has('category')->get() as $product)
                         @include('components.product',['product'=>$product,'inventory'=>$inventory])
