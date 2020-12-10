9821eb75f68572597a26fa3fa6570579be05f467
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-12-09 19:30:15 -0800

    code fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,9 @@
 @section('content')
     <body>
         <div class="container">
+
+            @include('components.alert')
+
             <div >
                 <img src="{{asset('image/landing.jpg')}}" class="img-fluid" alt="100%x250" style="height:100%; width: 100%; display: block;" >
             </div>
@@ -9,7 +12,7 @@
 
 
             @foreach(App\Category::with(['products'])->has('products')->get() as $category)
-            
+
                 <div class="col-md-12 mb-5 mt-5">
                     <div class="h2 text-center">{{ $category->name }}</div>
                 </div>
