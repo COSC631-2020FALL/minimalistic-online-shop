8572c3ab74c22df98fe2bec3e7a59054e8617160
greg <goyafuso.voided@gmail.com>
2020-12-10 04:05:23 +0000

    removed registration email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        Mail::to($data['email'])->queue(new UserRegistration());
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
